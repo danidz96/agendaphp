818e6e26ce5bf919daea9b33df2f7df82e7d982a
unknown <dani.d.zamorano@gmail.com>
2018-03-27 18:39:13 +0200

    Creación archivo crear.php y comprobar variables

---
+++ crear.php
@@ -0,0 +1,37 @@
+<?php
+  if (isset(_POST['nombre'])) {
+    $nombre = $_POST['nombre'];
+  }
+  if (isset(_POST['numero'])) {
+    $numero = $_POST['numero'];
+  }
+  
+   try {
+        require_once('funciones/bd_conexion.php');
+   } catch (Exception $e) {
+         $error = $e->getMessage();
+   }
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Agenda PHP</title>
+    <link rel="stylesheet" href="css/estilos.css" media="screen">
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Agenda de Contactos</h1>
+        <div class="contenido">
+
+
+        </div> <!--.contenido-->
+
+    </div>
+
+</body>
+</html>
